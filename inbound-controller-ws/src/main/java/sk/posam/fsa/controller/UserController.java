d14e0b4b385294422bb054a222f58a37ecb8f380
Zoltan Szitas <xszitasz@gmail.com>
2024-04-25 19:54:15 +0200

    get logged in user added

--- inbound-controller-ws/src/main/java/sk/posam/fsa/controller/UserController.java
+++ inbound-controller-ws/src/main/java/sk/posam/fsa/controller/UserController.java
@@ -2,13 +2,28 @@ package sk.posam.fsa.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import sk.posam.fsa.mapper.UserMapper;
 import sk.posam.fsa.rest.dto.UserDto;
+import sk.posam.fsa.security.CurrentUserDetailService;
 
 @RestController
 public class UserController implements sk.posam.fsa.rest.api.UsersApi{
+    private final UserMapper userMapper;
+    private final CurrentUserDetailService currentUserDetailService;
+
+    public UserController(UserMapper userMapper, CurrentUserDetailService currentUserDetailService) {
+        this.userMapper = userMapper;
+        this.currentUserDetailService = currentUserDetailService;
+    }
+
     @Override
     public ResponseEntity<UserDto> createUser(UserDto userDto) {
         return null;
+    }
+
+    @Override
+    public ResponseEntity<UserDto> getUserByEmail(String email) {
+        return ResponseEntity.ok(userMapper.toUserDto(currentUserDetailService.getFullCurrentUser()));
     }
 
     @Override
