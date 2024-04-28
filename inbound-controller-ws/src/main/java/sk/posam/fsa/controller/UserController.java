46fa7eedada3c7990ff1b31bdb97fa2542072074
Zoltan Szitas <xszitasz@gmail.com>
2024-04-28 10:34:37 +0200

    create user added

--- inbound-controller-ws/src/main/java/sk/posam/fsa/controller/UserController.java
+++ inbound-controller-ws/src/main/java/sk/posam/fsa/controller/UserController.java
@@ -1,24 +1,39 @@
 package sk.posam.fsa.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import sk.posam.fsa.User;
 import sk.posam.fsa.mapper.UserMapper;
 import sk.posam.fsa.rest.dto.UserDto;
+import sk.posam.fsa.rest.dto.UserRoleDto;
 import sk.posam.fsa.security.CurrentUserDetailService;
+import sk.posam.fsa.service.UserFacade;
 
 @RestController
 public class UserController implements sk.posam.fsa.rest.api.UsersApi{
     private final UserMapper userMapper;
     private final CurrentUserDetailService currentUserDetailService;
+    private final UserFacade userFacade;
 
-    public UserController(UserMapper userMapper, CurrentUserDetailService currentUserDetailService) {
+    public UserController(UserMapper userMapper, CurrentUserDetailService currentUserDetailService, UserFacade userFacade) {
         this.userMapper = userMapper;
         this.currentUserDetailService = currentUserDetailService;
+        this.userFacade = userFacade;
     }
 
     @Override
     public ResponseEntity<UserDto> createUser(UserDto userDto) {
-        return null;
+        UserRoleDto userRole = currentUserDetailService.getCurrentUser().getRole();
+
+        if (userRole != UserRoleDto.ADMIN) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+
+        User user = userMapper.toUserEntity(userDto);
+        user.setId(null);
+        userFacade.create(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(null);
     }
 
     @Override
